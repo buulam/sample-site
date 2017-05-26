86032cbbc9e6123d680797c0aaa9d06e2b32a464
buulam <buulam@gmail.com>
2017-05-26 15:23:48 -0700

    fixed typo in background color tag

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
   </title>
 </head>
 
-<body style="background-color:##34495E">
+<body style="background-color:#34495E">
   <font face="verdana" color="#D7DBDD">
   Hello World <br>
 Your IP address is <?php echo $_SERVER["REMOTE_ADDR"]; ?>
